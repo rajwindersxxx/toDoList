d94b4b35c9896b399ff20320bee96d0c31714411
rajwindersxxx <rajwindersxxx@gmail.com>
2024-12-04 01:14:19 +0530

    seperate icons svgs

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,6 +2,13 @@ import { useState } from 'react';
 import { Button } from './Button';
 import { AnimatePresence, motion } from 'motion/react';
 import { TaskDateAgo } from './TaskDateAgo';
+import {
+  CirclePlusIcon,
+  CircleTickIcon,
+  DeleteIcon,
+  EditIcon,
+  TickIcon,
+} from './Icons';
 interface props {
   title: string;
   note: string;
@@ -44,7 +51,7 @@ export function Task({
               <motion.input
                 initial={{ height: 28, opacity: 0 }}
                 animate={{ height: 30, opacity: 1 }}
-                exit={{ height: 28, opacity: 0 }}
+                exit={{ height: 28, opacity: 0, transition: { duration: 0.2 } }}
                 type="text"
                 key="edit"
                 className="rounded bg-purple-500 border-b w-full p-1 text-white"
@@ -65,26 +72,11 @@ export function Task({
           </AnimatePresence>
           {!isDone ? (
             <Button onClick={() => onTaskStatusChange(id)}>
-              <svg
-                className="float-end"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-              >
-                <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-              </svg>
+              <CirclePlusIcon />
             </Button>
           ) : (
             <Button onClick={() => onTaskStatusChange(id)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-              >
-                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-              </svg>
+              <TickIcon />
             </Button>
           )}
         </div>
@@ -115,37 +107,16 @@ export function Task({
           <div className="flex gap-2">
             {editMode ? (
               <Button onClick={handleUpdateForm}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                >
-                  <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-                </svg>
+                <CircleTickIcon />
               </Button>
             ) : (
               <Button onClick={() => setEditMode(!editMode)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                >
-                  <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
-                </svg>
+                <EditIcon />
               </Button>
             )}
 
             <Button className="p-0" onClick={() => onTaskDelete(id)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-              >
-                <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-              </svg>
+              <DeleteIcon />
             </Button>
           </div>
         </div>
